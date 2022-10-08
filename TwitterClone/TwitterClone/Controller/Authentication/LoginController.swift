42002099f8a1282c18ef222734f60a03a2498def
Ali Eldeeb <aeldeeb@fordham.edu>
2022-10-07 19:10:31 -0700

    Added email and password textfield to loginScreen and some UIView and Textfield extensions

--- TwitterClone/TwitterClone/Controller/Authentication/LoginController.swift
+++ TwitterClone/TwitterClone/Controller/Authentication/LoginController.swift
@@ -17,6 +17,32 @@ class LoginController: UIViewController{
         iv.image = #imageLiteral(resourceName: "TwitterLogo")
         return iv
     }()
+    
+    private lazy var emailContainerView: UIView = {
+        let view = UIView().inputContainerView(withImage: #imageLiteral(resourceName: "ic_mail_outline_white_2x-1"), textfield: emailTextField)
+        return view
+    }()
+    
+    //this email text field will be added to our email ContainerView, we made our emailTextField outside the container view bc eventually we are going to need to grab the text from it later.
+    private lazy var emailTextField: UITextField = {
+        var tf = UITextField().textField(withPlaceholder: "Email", isSecureTextEntry: false)
+        tf.tag = 1
+        tf.delegate = self
+        return tf
+    }()
+    
+    private lazy var passwordContainerView: UIView = {
+        let view = UIView().inputContainerView(withImage: #imageLiteral(resourceName: "ic_lock_outline_white_2x"),textfield: passwordTextField)
+        return view
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let tf =  UITextField().textField(withPlaceholder: "Password", isSecureTextEntry: true)
+        tf.tag = 2
+        tf.delegate = self
+        return tf
+    }()
+
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +56,19 @@ class LoginController: UIViewController{
         navigationController?.navigationBar.isHidden = true
         view.addSubview(logoImageView)
         logoImageView.centerX(inView: view, topAnchor: view.safeAreaLayoutGuide.topAnchor)
-        logoImageView.setDimensions(height: 185, width: 185)
+        logoImageView.setDimensions(height: 165, width: 165)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainerView, passwordContainerView])
+        stack.axis = .vertical
+        stack.spacing = 8
+        view.addSubview(stack)
+        stack.anchor(top: logoImageView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
+        
     }
     
     //MARK: - Selectors
+}
+
+extension LoginController: UITextFieldDelegate{
+    
 }
--- TwitterClone/TwitterClone/Utils/Extensions.swift
+++ TwitterClone/TwitterClone/Utils/Extensions.swift
@@ -8,7 +8,33 @@
 import UIKit
 
 extension UIView{
-    func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat = 0, paddingLeft: CGFloat = 0, paddingBottom: CGFloat = 0, paddingRight: CGFloat = 0){
+    
+    func inputContainerView(withImage image: UIImage, textfield: UITextField? = nil) -> UIView{
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        let imageView = UIImageView()
+        imageView.image = image
+        imageView.alpha = 0.87
+        view.addSubview(imageView)
+        
+        //imageview constraints will be different if the textField exists vs if the segmented control exits
+        if let textfield = textfield{
+            imageView.centerY(inView: view) //vertically centering it in the view
+            imageView.anchor(left: view.leftAnchor, paddingLeft: 8, height: 24, width: 24)
+            view.addSubview(textfield)
+            textfield.centerY(inView: view)
+            textfield.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        }
+        
+        let divider = UIView()
+        divider.backgroundColor = .white
+        view.addSubview(divider)
+        divider.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8,paddingRight: 24, height: 0.75)
+        
+        return view
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat = 0, paddingLeft: CGFloat = 0, paddingBottom: CGFloat = 0, paddingRight: CGFloat = 0, height: CGFloat? = nil, width: CGFloat? = nil){
         translatesAutoresizingMaskIntoConstraints = false
         if let top = top{
             topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
@@ -24,6 +50,14 @@ extension UIView{
         
         if let right = right{
             rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let height = height{
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+        if let width = width{
+            widthAnchor.constraint(equalToConstant: width).isActive = true
         }
     }
     
@@ -62,4 +96,29 @@ extension UIColor{
     
     static let twtrBlue = UIColor.rgb(red: 29, green: 161, blue: 242)
     
+}
+
+extension UITextField{
+    func textField(withPlaceholder placeholder: String, isSecureTextEntry: Bool) -> UITextField{
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.isSecureTextEntry = isSecureTextEntry
+        tf.keyboardAppearance = .dark //dark keybord to stick with the dark theme
+        //attributed placeholder with a custom color
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder,attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        return tf
+    }
+}
+
+extension UILabel{
+    func uiLabel(withText: String? = nil, font: UIFont?, textColor: UIColor) -> UILabel{
+        let label = UILabel()
+        label.text = withText
+        label.font = font
+        label.textColor = textColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
 }
