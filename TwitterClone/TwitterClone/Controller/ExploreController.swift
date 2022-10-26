195f40093569056f31e6200d2509e3054ca29474
Ali Eldeeb <aeldeeb@fordham.edu>
2022-10-25 19:29:11 -0700

    Feat: Setting up ExploreController to show all existing users in database

--- TwitterClone/TwitterClone/Controller/ExploreController.swift
+++ TwitterClone/TwitterClone/Controller/ExploreController.swift
@@ -6,20 +6,45 @@
 //
 
 import UIKit
-
-class ExploreController: UIViewController{
+private let reuseIdentifier = "reuseIdentifier"
+class ExploreController: UITableViewController{
     //MARK: - Properties
-    
+    private var users = [User](){
+        didSet{
+            tableView.reloadData()
+        }
+    }
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        
+        fetchUsers()
     }
     
+    //MARK: - API
+    private func fetchUsers(){
+        UserService.shared.fetchUsers { users in
+            self.users = users
+        }
+    }
     //MARK: - Helpers
     func configureUI(){
         view.backgroundColor = .white
         navigationItem.title = "Explore"
+        tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 60
+        //removing serpator lines
+        tableView.separatorColor = .none
+    }
+}
+
+extension ExploreController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        return cell
     }
 }
---
+++ TwitterClone/TwitterClone/View/UserCell.swift
@@ -0,0 +1,54 @@
+//
+//  UserCell.swift
+//  TwitterClone
+//
+//  Created by Ali Eldeeb on 10/25/22.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell{
+    //MARK: - Properties
+    private lazy var profileImageView : UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.setDimensions(height: 40, width: 40)
+        iv.layer.cornerRadius = 20
+        iv.layer.masksToBounds = true
+        iv.backgroundColor = .twtrBlue
+    
+        return iv
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.text = "Username"
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Fullname"
+        return label
+    }()
+    //MARK: - LifeCycle
+    //how you initialize a uitableviewcell programmatically
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(profileImageView)
+        profileImageView.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+        let nameStack = UIStackView(arrangedSubviews: [usernameLabel, fullnameLabel])
+        nameStack.axis = .vertical
+        nameStack.spacing = 3
+        nameStack.distribution = .fillProportionally
+        addSubview(nameStack)
+        nameStack.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
